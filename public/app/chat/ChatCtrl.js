e625962bd1369e44ca104f49f1018df76d1fe653
Vitaly <motor.kodlife@gmail.com>
2014-08-04 11:39:54 -0500

    fix(ChatCtrl): fixed linting issues

--- public/app/chat/ChatCtrl.js
+++ public/app/chat/ChatCtrl.js
@@ -1,27 +1,28 @@
 "use strict";
 
-angular.module('tcApp').controller('ChatCtrl', function($scope, Chat, Identity, Notifier, ChatResource){
+angular.module('tcApp')
+  .controller('ChatCtrl', function($scope, Chat, Identity, Notifier, ChatResource){
 
-  $scope.identity = Identity;
-  $scope.chat = {};
-  $scope.chat.messages = ChatResource.query()
+    $scope.identity = Identity;
+    $scope.chat = {};
+    $scope.chat.messages = ChatResource.query();
 
-  $scope.sendMessage = function(author, message){
-    var user = author.firstName + ' ' + author.lastName;
-    var newMessage = {
-      author: user,
-      text:message,
-      date: new Date()
+    $scope.sendMessage = function(author, message){
+      var user = author.firstName + ' ' + author.lastName;
+      var newMessage = {
+        author: user,
+        text:message,
+        date: new Date()
+      };
+
+      Chat.sendMessage(newMessage).then(function(success){
+        if(success){
+          $scope.chat.messages.push(newMessage);
+          $scope.chat.message = '';
+        } else {
+          Notifier.error('Could not send your message!');
+        }
+      });
     };
 
-    Chat.sendMessage(newMessage).then(function(success){
-      if(success){
-        $scope.chat.messages.push(newMessage);
-        $scope.chat.message = '';
-      } else {
-        Notifier.error('Could not send your message!');
-      }
-    });
-  };
-
-});
+  });
