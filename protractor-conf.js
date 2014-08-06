ba8e261b2ec55a864c754a3b4ff092775ffa3997
Vitaly <motor.kodlife@gmail.com>
2014-08-06 13:54:24 -0500

    feat(e2e test): added basic E2E tests

---
+++ protractor-conf.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.config = {
+
+  specs:['test/e2e/**/*.js'],
+
+  baseUrl: 'http://localhost:8080',
+
+  framework: 'jasmine'
+
+};
---
+++ test/e2e/index/index_spec.js
@@ -0,0 +1,23 @@
+describe("Pages",function(){
+
+  describe("Index page",function(){
+
+    it("has correct title", function(){
+      browser.get('/');
+      expect(browser.getTitle()).toBe('teamcity-demo-project');
+    });
+
+    it("has correct jumbotron message", function(){
+      var message = element(by.css('.jumbotron h1'));
+      expect(message.getText()).toBe('TeamCity test project!');
+    });
+
+    it("has correct Sign In button text", function(){
+      var btn = element(by.tagName('button'));
+      expect(btn.getText()).toBe('Sign In');
+    });
+
+  });
+
+
+});
